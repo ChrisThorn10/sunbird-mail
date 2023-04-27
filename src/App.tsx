b118f4ad73743d0a7d2404df4015a8d703c21a50
Christie <thornton.christie@gmail.com>
2023-04-26 23:11:58 -0400

    Drag and drop (#3)
    * add itnital drag drop functionality
    
    * revert changes to original drag and drop
    
    ---------
    
    Co-authored-by: Christie Thornton <christiethornton@Christies-MacBook-Pro.local>

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import ContentSection from './ContentSection';
 import Layout from './Layout';
+import Hero1 from '../src/assets/layouts/hero-1.jpg'
+
 
 function App() {
-  let contentSections: React.ReactElement[] = [];
+  let contentSections : React.ReactElement[] = [];
   const [numContentSections, setNumContentSections] = useState(1)
   
   const addContentSection = () => {
@@ -31,14 +33,25 @@ function App() {
             addContent={addContentSection} 
             removeContent={removeContentSection} 
             numContentSections={numContentSections}
+            applyContent={applyContent}
             />
         );
       };
 
+  function applyContent(i : number, layout : any) {
+    const newElem = new Image(600, 300);
+    newElem.src=Hero1;
+    let elem = document.getElementById(`contentSection${i}`)?.replaceChildren(newElem)
+  }
+
   return (
     <div className="App">
       <div id="AppLayoutContainer">
-        <Layout />
+        <div className="Layout" style={{overflow: "auto"}}>
+          <h1>Layout Panel</h1>
+          <p>Select a Layout</p>
+            <Layout />
+        </div>
       </div>
       <div id="AppTemplateContainer">
       {contentSections}
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,22 +1,21 @@
 
 import React, { useState, useEffect } from 'react';
 import Hero1 from '../src/assets/layouts/hero-1.jpg'
+import { useDrag } from "react-dnd";
+import { isPropertySignature } from 'typescript';
 
-function Layout() {
+const Layout = () => {
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: "layout",
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
   return (
-    <div className="Layout" style={{overflow: "auto"}}>
-      <h1>Layout Panel</h1>
-      <p>Select a Layout</p>
-      <div draggable>
-        <img src={Hero1} alt="fireSpot" style={{width: "60%", paddingBottom: "10px"}} />
+      <div ref={dragRef}>
+        <img src={Hero1} alt="Hero1" style={{width: "60%", marginBottom: "10px", border: isDragging ? "10px solid aquamarine" : "none"}} />
       </div>
-      <div draggable>
-        <img src={Hero1} alt="fireSpot" style={{width: "60%", paddingBottom: "10px"}} />
-      </div>
-      <div draggable>
-        <img src={Hero1} alt="fireSpot" style={{width: "60%", paddingBottom: "10px"}} />
-      </div>
-    </div>
   );
 }
 
