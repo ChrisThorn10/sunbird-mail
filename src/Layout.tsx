59d94b3b2c6eb61d678cd3f31a85f09421396460
Christie Thornton <christiethornton@Christies-MacBook-Pro.local>
2023-05-03 16:32:56 -0700

    complete basic drag and drop functionality

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,21 +1,24 @@
 
 import React, { useState, useEffect } from 'react';
-import Hero1 from '../src/assets/layouts/hero-1.jpg'
 import { useDrag } from "react-dnd";
 import { isPropertySignature } from 'typescript';
 
-const Layout = () => {
-  const [{ isDragging }, dragRef] = useDrag(() => ({
+
+const Layout = (props : any) => {
+
+  // Drag Hook.  When a layout is dragged over a content section it passes the properties of that item
+  const [{ isDragging }, dragRef] = useDrag({
     type: "layout",
+    item: { props },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
-    }),
-  }));
+    })
+  });
 
   return (
       <div ref={dragRef}>
-        <img src={Hero1} alt="Hero1" style={{width: "60%", marginBottom: "10px", border: isDragging ? "10px solid aquamarine" : "none"}} />
-      </div>
+          <img src={props.imgSrc} alt={props.imgAlt} style={{width: "60%", marginBottom: "10px", border: isDragging ? "10px solid aquamarine" : "none"}} />
+      </div>      
   );
 }
 
